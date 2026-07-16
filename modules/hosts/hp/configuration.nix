37b3a8cdb886708b5cf97b4bb41078aafcc160f6
zentag <38821230+zentag@users.noreply.github.com>
2026-07-16 02:57:46 -0400

    merge

--- modules/hosts/hp/configuration.nix
+++ modules/hosts/hp/configuration.nix
@@ -25,7 +25,6 @@
       "eDP-1, 1920x1080, 0x0, 1"
     ];
     services.udisks2.enable = true;
-    services.udiskie.enable = true;
     wifi.enable = true;
     username = "zen";
     system.stateVersion = "23.11"; # absolutely do not change
@@ -36,7 +35,15 @@
     # You should not change this value, even if you update Home Manager. If you do
     # want to update the value, then make sure to first check the Home Manager
     # release notes.
-    home-manager.users.zen.home.stateVersion = "24.05"; # Please read the comment before changing.
+    home-manager.users.zen = {
+      home.stateVersion = "24.05"; # Please read the comment before changing.
+      services.udiskie = {
+        enable = true;
+        automount = true;
+        notify = true;
+        tray = "never"; # or "auto" if you want a tray icon
+      };
+    };
     users.users = {
       zen = {
         isNormalUser = true;
