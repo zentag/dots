579d8b3c736e459f532ff73e99178d20ba96df55
zentag <38821230+zentag@users.noreply.github.com>
2026-09-18 13:55:51 -0400

    fix java fmt

--- nvf/format.nix
+++ nvf/format.nix
@@ -1,13 +1,16 @@
-{
-  vim.formatter.conform-nvim = {
-    enable = true;
-    # format java like wpilib
-    setupOpts = {
-      formatters_by_ft.java = ["google-java-format"];
-      formatters.google-java-format = {
-        command = "google-java-format";
-        args = ["--aosp" "-"];
-        stdin = true;
+{pkgs, ...}: {
+  vim = {
+    extraPackages = [pkgs.google-java-format];
+    formatter.conform-nvim = {
+      enable = true;
+      # format java like wpilib
+      setupOpts = {
+        formatters_by_ft.java = ["google-java-format"];
+        formatters.google-java-format = {
+          command = "${pkgs.google-java-format}/bin/google-java-format";
+          args = ["--aosp" "-"];
+          stdin = true;
+        };
       };
     };
   };
