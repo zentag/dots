21488129b58857e2091ec364c446261cb402e36d
zentag <38821230+zentag@users.noreply.github.com>
2026-09-22 13:15:16 -0400

    try to make frc sim work

--- modules/features/frc.nix
+++ modules/features/frc.nix
@@ -3,10 +3,25 @@
   inputs,
   ...
 }: {
-  flake.nixosModules.frc = {config, ...}: {
+  flake.nixosModules.frc = {
+    config,
+    pkgs,
+    ...
+  }: {
     imports = [
       (self.lib.hm config.username "frc")
     ];
+
+    # GradleRIO downloads prebuilt WPILib/vendor JNI libraries for `simulateJava`
+    # that expect an FHS layout, so they can't find libstdc++ etc. on NixOS.
+    # nix-ld gives them a working dynamic loader instead.
+    # Docs: https://github.com/frc4451/frc-nix/blob/season/2026/docs/simulation-gui.md
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        libGL # needed if the WPILib sim GUI window is opened
+      ];
+    };
   };
   flake.homeModules.frc = {pkgs, ...}: {
     home.packages = with inputs.frc.packages.${pkgs.stdenv.hostPlatform.system}; [
