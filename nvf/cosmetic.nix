be7afe79ef9e5674d94267d120f36d3b993d1d01
zentag <38821230+zentag@users.noreply.github.com>
2026-09-22 13:14:36 -0400

    make notifiy be in bottom right (nvf)

--- nvf/cosmetic.nix
+++ nvf/cosmetic.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   vim = {
     # this is shown on startup if you don't provide any args
     dashboard.alpha = {
@@ -17,7 +17,16 @@
     };
     mini = {
       icons.enable = true;
-      notify.enable = true;
+      notify = {
+        enable = true;
+        setupOpts.window.config = lib.generators.mkLuaInline ''
+          function()
+            local has_statusline = vim.o.laststatus > 0
+            local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+            return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - pad }
+          end
+        '';
+      };
     };
     # do current line blames and other stuff if you want
     git.gitsigns.enable = true;
